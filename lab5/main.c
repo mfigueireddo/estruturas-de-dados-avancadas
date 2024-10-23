272a2aff16c5ce415745244ad82b6cbae884b380
mfigueireddo <matheusmsfigueiredo@gmail.com>
2024-10-23 10:51:10 -0300

    Começo laboratório 5

---
+++ lab5/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "aux.h"
+
+void soma_um(long *vetor);
+
+int main(void){
+
+    long vetor[1000];
+
+    long aux;
+    int indice = 0;
+
+    FILE* arquivo = fopen("cpf_list.txt", "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    while (fscanf(arquivo, " %ld", &aux) == 1) {
+        vetor[indice++] = aux;
+    }
+
+    soma_um(vetor);
+
+}
